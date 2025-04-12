91501db60ce9c5643ffff9c31ff10bdfc205de65
vickydadon <oluwatosin.omiteru@ucdenver.edu>
2025-04-12 17:48:26 -0600

    updated signin

--- server.js
+++ server.js
@@ -65,28 +65,28 @@ router.post('/signup', function(req, res) {
     }
 });
 
-router.post('/signin', function (req, res) {
-    var userNew = new User();
-    userNew.username = req.body.username;
-    userNew.password = req.body.password;
-
-    User.findOne({ username: userNew.username }).select('name username password').exec(function(err, user) {
-        if (err) {
-            res.send(err);
-        }
-
-        user.comparePassword(userNew.password, function(isMatch) {
-            if (isMatch) {
-                var userToken = { id: user.id, username: user.username };
-                var token = jwt.sign(userToken, process.env.SECRET_KEY);
-                res.json ({success: true, token: 'JWT ' + token});
-            }
-            else {
-                res.status(401).send({success: false, msg: 'Authentication failed.'});
-            }
-        })
-    })
-});
+router.post('/signin', async (req, res) => { // Use async/await
+    try {
+      const user = await User.findOne({ username: req.body.username }).select('name username password');
+  
+      if (!user) {
+        return res.status(401).json({ success: false, msg: 'Authentication failed. User not found.' }); // 401 Unauthorized
+      }
+  
+      const isMatch = await user.comparePassword(req.body.password); // Use await
+  
+      if (isMatch) {
+        const userToken = { id: user._id, username: user.username }; // Use user._id (standard Mongoose)
+        const token = jwt.sign(userToken, process.env.SECRET_KEY, { expiresIn: '1h' }); // Add expiry to the token (e.g., 1 hour)
+        res.json({ success: true, token: 'JWT ' + token });
+      } else {
+        res.status(401).json({ success: false, msg: 'Authentication failed. Incorrect password.' }); // 401 Unauthorized
+      }
+    } catch (err) {
+      console.error(err); // Log the error
+      res.status(500).json({ success: false, message: 'Something went wrong. Please try again later.' }); // 500 Internal Server Error
+    }
+  });
 
 // GET all reviews
 router.get('/reviews', function (req, res) {
